6b972bb102ed470bd6a020b94ac54d35c8631922
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-05 17:29:17 +0700

    Create PembayaranGaji.java

---
+++ Latihan 3/PembayaranGaji.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+class PembayaranGaji {
+    public int hitungGaji(Pegawai peg){
+        int uang=peg.gaji();
+        if(peg instanceof Direktur)
+            uang+=((Direktur)peg).tunjangan();
+        if(peg instanceof Staf)
+            uang+=((Staf)peg).Bonus();
+        return uang;
+    }
+    public static void main(String[]args){
+        PembayaranGaji pg=new PembayaranGaji();
+        Staf ali=new Staf();
+        Direktur tony = new Direktur();
+        System.out.println("Gaji yang dibayarkan untuk Staf ="+pg.hitungGaji(ali));
+        System.out.println("Gaji yang dibayarkan kepada Direktur ="+pg.hitungGaji(tony));
+    }
+}
