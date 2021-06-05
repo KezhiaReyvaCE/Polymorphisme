bc6e7d254ec344f84b93efcc1c41bc9bd5f1bc5f
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-05 14:16:56 +0700

    Create Override.java

---
+++ Latihan 1/Override.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class AeroPlan extends Vehicle{
+    @Override
+    public void fuel(){
+        System.out.println("fuels");
+    }
+    
+    public void walk (){
+        System.out.println("Aeroplan is flying");
+        }
+    
+    public static void main (String []args){
+        AeroPlan garuda = new AeroPlan();
+        garuda.function();
+        garuda.fuel();
+        garuda.walk();
+    }
+}
