94e535c2dd810964bce414484997b7570c83c432
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-07 01:25:03 +0700

    Create OperasiPenjumlahan.java

---
+++ Tugas Tambahan/OperasiPenjumlahan.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+class OperasiPenjumlahan extends OperasiBilanganAbs{ 
+    @Override
+    protected void set_A(double a){
+    this.a = a; }
+    @Override
+    protected void set_B(double b){
+    this.b = b; }
+    @Override
+    protected void set_C(){
+    this.c = c; }
+        
+    @Override
+    protected double get_A(){
+        return this.a;}
+    @Override
+    protected  double get_B(){
+        return this.b;}
+    @Override
+    protected  double get_C(){
+        return this.c;}
+    
+    @Override
+    protected  void tampil(){
+    this.tampil();
+    }
+    
+    
+    public double hasil(){
+          double hasil = (double)(this.a + this.b);
+          return hasil;
+      }
+    }
+
