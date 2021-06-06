9d622a7c37d2d1ac4a8fa4ed4d443d87e4e48298
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-07 01:28:25 +0700

    Create OperasiBilanganAbsCetak.java

---
+++ Tugas Tambahan/OperasiBilanganAbsCetak.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+final class OperasiBilanganAbsCetak {
+    private void cetakSemua(OperasiBilanganAbs[]OB,double a,double b){
+       
+    }
+}
